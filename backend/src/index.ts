c2a8a2148fc0f273e818fccb047eb8b8a9610351
Home PC <vfxprojectguy@gmail.com>
2024-04-15 21:50:30 -0700

    add listen port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.post("/api/chat", (req, res) => {
+app.post("/hello", (req, res) => {
   res.send("Hello World!");
+});
+
+app.listen(7890, () => {
+  console.log("Server started on port 7890");
 });
